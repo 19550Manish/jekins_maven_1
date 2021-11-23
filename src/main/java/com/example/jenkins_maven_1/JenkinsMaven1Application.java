39559d4e01489d215df956ca870ce4a7b43132f0
19550Manish <91590633+19550Manish@users.noreply.github.com>
2021-11-24 00:27:42 +0530

    Add files via upload

---
+++ src/main/java/com/example/jenkins_maven_1/JenkinsMaven1Application.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_maven_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsMaven1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsMaven1Application.class, args);
+	}
+
+}
